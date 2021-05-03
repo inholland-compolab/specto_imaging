8f972cc1c3b678b3d829a58f16ac2e98ba8df4cd
J5ST5CE <82508472+J5ST5CE@users.noreply.github.com>
2021-05-03 13:01:36 +0200

    Add files via upload

---
+++ image-stitching 2.py
@@ -0,0 +1,24 @@
+import cv2
+import os
+mainfolder = r"C:\Image stitching test\images"
+mainoutput = r"C:\Image stitching test\output"
+myfolders = os.listdir(mainfolder)
+print(myfolders)
+
+for folder in myfolders:
+    path = mainfolder+'/'+folder
+    images = []
+    myList = os.listdir(path)
+    for imgN in myList:
+        curImg = cv2.imread(f'{path}/{imgN}')
+        curImg = cv2.resize(curImg,(0,0),None,0.2,0.2)
+        images.append(curImg)
+        sticher = cv2.Stitcher.create()
+        (status,results) = sticher.stitch(images)
+        if (status == cv2.Stitcher_OK):
+            print('Stitching succesful')
+            cv2.imshow(folder,results)
+            cv2.waitKey(1)
+        else:
+            print("Stitching failed")
+cv2.waitKey(0)
