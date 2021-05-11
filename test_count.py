8f8cf7dab1e570d712e840415ba1af24a6a5d2a1
eurekatm <81294489+eurekatm@users.noreply.github.com>
2021-05-11 14:35:50 +0200

    Create test_count.py

---
+++ test_count.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_math_method1():
+    x=5
+    y=6
+    assert x+1 == y, "test failed"
+    assert x == y, "test failed"
+
+def test_math_method2():
+    x=5
+    y=6
+    assert x+1 == y, "test failed"
